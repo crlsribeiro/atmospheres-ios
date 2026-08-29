280f0911c9a78cdf369108bbbf41ef0d8e184a98
carlosribeiro <carlos.a.o.r@icloud.com>
2026-08-29 08:15:06 -0500

    feat: initial Xcode project scaffold

---
+++ Atmospheres/Atmospheres/AtmospheresApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmospheresApp.swift
+//  Atmospheres
+//
+//  Created by Carlos Ribeiro on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AtmospheresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
